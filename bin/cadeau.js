7c923fa6c41b95b46915669f3018ddae2685ef2b
Michael Tiller <michael.tiller@gmail.com>
2014-02-25 16:25:42 -0500

    Added a bunch of documentation

--- bin/cadeau.js
+++ bin/cadeau.js
@@ -1,25 +1,36 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
+/* Load dependencies */
+var fs = require('fs-extra');
 var yaml = require('js-yaml');
 var cadeau = require('..');
 var path = require('path');
 
+/* Check the arguments.
+   TODO: Add argparse support at some point. */
 if (process.argv.length!=4) {
     throw "Usage: "+process.argv[1]+" slides.yaml output.html";
 }
 
+/* Pull out arguments */
 var src = process.argv[2];
 var dst = process.argv[3];
 
+/* Read the presentation file */
 var raw = fs.readFileSync(src, "utf8");
+/* Transform it into a data structure. */
 var data = yaml.load(raw, { filename: src });
 
-var preamble = "";
-var context = cadeau.processSlides(data, preamble);
+var context = cadeau.processSlides(data);
 
-try {
-    fs.mkdirSync(dst)
-} catch(e) {}
+/* Make the directory, if we didn't already. */
+fs.mkdirsSync(dst);
+/* Run the code generator for the backend engine */
 cadeau.generate(context, dst);
+/* Copy presentation specific resources.
+
+   Note, this is deliberatly done second to allow the presentation
+   to insert its own resources to overwrite those added by
+   the backend engine.
+ */
 cadeau.copyResources(data, dst);
